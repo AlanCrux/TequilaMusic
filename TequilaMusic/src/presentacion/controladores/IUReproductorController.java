573aef2c938ed00d42757c7bcc4f080b9641bb7f
Alan Yoset García Cruz <alancrux_@hotmail.com>
2018-05-23 10:56:25 -0500

    Cambios ultra menores
    Se suben cambios menores para comprobar la responsividad en máquinas distintas.

--- TequilaMusic/src/presentacion/controladores/IUReproductorController.java
+++ TequilaMusic/src/presentacion/controladores/IUReproductorController.java
@@ -40,216 +40,205 @@ import javafx.stage.Stage;
  */
 public class IUReproductorController implements Initializable {
 
-    @FXML
-    private TextField tfBuscar;
-    @FXML
-    private Slider sliderVolumen;
-    @FXML
-    private SplitPane splitPane;
-    @FXML
-    private AnchorPane paneContenido;
-    @FXML
-    private ImageView btnAleatorio;
-    @FXML
-    private ImageView btnAnterior;
-    @FXML
-    private ImageView btnPlay;
-    @FXML
-    private ImageView btnSiguiente;
-    @FXML
-    private ImageView btnRepetir;
-    @FXML
-    private JFXListView<Label> listOpciones;
-    @FXML
-    private JFXButton btnBiblioteca;
-    @FXML
-    private JFXButton btnRadio;
-    @FXML
-    private JFXButton btnExplorar;
-    @FXML
-    private JFXNodesList nodeListAjustes;
-    @FXML
-    private JFXButton btnAjustes;
-    @FXML
-    private JFXButton btnUsuario;
-    @FXML
-    private JFXButton btnConfigurar;
-    @FXML
-    private ImageView imgDisco;
-    @FXML
-    private Label lbNombreCancion;
-    @FXML
-    private Label lbArtistas;
-    @FXML
-    private ImageView imgDetallesDisco;
+  @FXML
+  private TextField tfBuscar;
+  @FXML
+  private Slider sliderVolumen;
+  @FXML
+  private SplitPane splitPane;
+  @FXML
+  private AnchorPane paneContenido;
+  @FXML
+  private ImageView btnAleatorio;
+  @FXML
+  private ImageView btnAnterior;
+  @FXML
+  private ImageView btnPlay;
+  @FXML
+  private ImageView btnSiguiente;
+  @FXML
+  private ImageView btnRepetir;
+  @FXML
+  private JFXListView<Label> listOpciones;
+  @FXML
+  private JFXButton btnBiblioteca;
+  @FXML
+  private JFXButton btnRadio;
+  @FXML
+  private JFXButton btnExplorar;
+  @FXML
+  private JFXNodesList nodeListAjustes;
+  @FXML
+  private JFXButton btnAjustes;
+  @FXML
+  private JFXButton btnUsuario;
+  @FXML
+  private JFXButton btnConfigurar;
+  @FXML
+  private ImageView imgDisco;
+  @FXML
+  private Label lbNombreCancion;
+  @FXML
+  private Label lbArtistas;
+  @FXML
+  private ImageView imgDetallesDisco;
 
-    private boolean play = true;
-    private static final String ICON_CANCIONES = "src/recursos/iconos/maracas.png";
-    private static final String ICON_ARTISTAS = "src/recursos/iconos/mexicano.png";
-    private static final String ICON_ALBUMES = "src/recursos/iconos/guitarra.png";
-    private static final String ICON_PAUSE = "/recursos/iconos/icon_pausa.png";
-    private static final String ICON_PLAY = "/recursos/iconos/icon_play.png";
-    private static final String ICON_PLAYLIST = "src/recursos/iconos/icon_playlist.png";
+  private boolean play = true;
+  private static final String ICON_CANCIONES = "src/recursos/iconos/maracas.png";
+  private static final String ICON_ARTISTAS = "src/recursos/iconos/mexicano.png";
+  private static final String ICON_ALBUMES = "src/recursos/iconos/guitarra.png";
+  private static final String ICON_PAUSE = "/recursos/iconos/icon_pausa.png";
+  private static final String ICON_PLAY = "/recursos/iconos/icon_play.png";
+  private static final String ICON_PLAYLIST = "src/recursos/iconos/icon_playlist.png";
 
-    /**
-     * Inicializa los componentes de la ventana IUReproductor.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        agregarOpcionesBiblioteca();
-        agregarPlaylists();
+  /**
+   * Inicializa los componentes de la ventana IUReproductor.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    agregarOpcionesBiblioteca();
+    agregarPlaylists();
 
-        List<JFXButton> botones = new ArrayList<>();
-        botones.add(btnConfigurar);
-        botones.add(btnUsuario);
-        botones.add(btnAjustes);
-        llenarNodeList(nodeListAjustes, botones);
+    List<JFXButton> botones = new ArrayList<>();
+    botones.add(btnConfigurar);
+    botones.add(btnUsuario);
+    botones.add(btnAjustes);
+  }
+
+  @FXML
+  private void mostrarDetallesDisco(MouseEvent event) {
+    imgDetallesDisco.setOpacity(0.8);
+  }
+
+  @FXML
+  private void ocultarDetallesDisco(MouseEvent event) {
+    imgDetallesDisco.setOpacity(0);
+  }
+
+  @FXML
+  private void onActionBack(MouseEvent event) {
+
+  }
+
+  @FXML
+  void onActionBiblioteca(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onActionBuscar(KeyEvent event) {
+
+  }
+
+  @FXML
+  void onActionNext(MouseEvent event) {
+
+  }
+
+  @FXML
+  void onActionPlay(MouseEvent event) {
+    if (play) {
+      btnPlay.setImage(new Image(ICON_PAUSE));
+      play = false;
+    } else {
+      btnPlay.setImage(new Image(ICON_PLAY));
+      play = true;
     }
+  }
 
-    /**
-     * Método que agrega botones a un NodeList.
-     */
-    private void llenarNodeList(JFXNodesList nodeList, List<JFXButton> elements) {
-        for (int i = 0; i < elements.size(); i++) {
-            nodeList.addAnimatedNode((JFXButton) elements.get(i));
-        }
-        nodeList.setSpacing(6);
+  @FXML
+  void onActionRadio(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onActionRandom(MouseEvent event) {
+
+  }
+
+  @FXML
+  void onActionRepeat(MouseEvent event) {
+
+  }
+
+  @FXML
+  void onActionTimeline(MouseEvent event) {
+
+  }
+
+  @FXML
+  void onActionVolume(MouseEvent event) {
+
+  }
+
+  private void agregarOpcionesBiblioteca() {
+    Label lbBiblioteca = new Label("Biblioteca");
+    Label lbCanciones = new Label("Canciones");
+    Label lbArtistas = new Label("Artistas");
+    Label lbAlbumes = new Label("Álbumes");
+    try {
+      lbCanciones.setGraphic(new ImageView(new Image(new FileInputStream(ICON_CANCIONES))));
+      lbArtistas.setGraphic(new ImageView(new Image(new FileInputStream(ICON_ARTISTAS))));
+      lbAlbumes.setGraphic(new ImageView(new Image(new FileInputStream(ICON_ALBUMES))));
+
+      lbBiblioteca.setFont(new Font("Avenir Book", 16));
+      lbCanciones.setFont(new Font("Avenir Book", 14));
+      lbArtistas.setFont(new Font("Avenir Book", 14));
+      lbAlbumes.setFont(new Font("Avenir Book", 14));
+
+      listOpciones.getItems().add(lbBiblioteca);
+      listOpciones.getItems().add(lbCanciones);
+      listOpciones.getItems().add(lbArtistas);
+      listOpciones.getItems().add(lbAlbumes);
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
     }
+  }
 
-    @FXML
-    private void mostrarDetallesDisco(MouseEvent event) {
-        imgDetallesDisco.setOpacity(0.8);
+  /**
+   * Nota: En el futuro debe recibir una lista cargada con las playlist
+   */
+  private void agregarPlaylists() {
+    Label lbPlaylist = new Label("Playlist");
+    lbPlaylist.setFont(new Font("Avenir Book", 16));
+    listOpciones.getItems().add(lbPlaylist);
+
+    //AQUI SE DEBERÍAN AGREGAR LAS PLAYLIST DEL USUARIO
+    Label lbCanciones = new Label("Carne asada");
+    Label lbArtistas = new Label("Estudiar");
+    Label lbAlbumes = new Label("Baño de burbujas");
+    try {
+      lbCanciones.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
+      lbArtistas.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
+      lbAlbumes.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
+
+      lbCanciones.setFont(new Font("Avenir Book", 14));
+      lbArtistas.setFont(new Font("Avenir Book", 14));
+      lbAlbumes.setFont(new Font("Avenir Book", 14));
+
+      listOpciones.getItems().add(lbCanciones);
+      listOpciones.getItems().add(lbArtistas);
+      listOpciones.getItems().add(lbAlbumes);
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
     }
+  }
 
-    @FXML
-    private void ocultarDetallesDisco(MouseEvent event) {
-        imgDetallesDisco.setOpacity(0);
+  /**
+   * Muestra la ventana.
+   *
+   * @param loader el loader con la ruta de la ventana que se quiere cargar.
+   */
+  public static void mostrarVentana(FXMLLoader loader) {
+    try {
+      Stage stagePrincipal = new Stage();
+      Parent root = (Parent) loader.load();
+      Scene scene = new Scene(root);
+      stagePrincipal.setScene(scene);
+      stagePrincipal.setMaximized(true);
+      stagePrincipal.show();
+    } catch (IOException ex) {
+      Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
     }
-
-    @FXML
-    private void onActionBack(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onActionBiblioteca(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onActionBuscar(KeyEvent event) {
-
-    }
-
-    @FXML
-    void onActionNext(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onActionPlay(MouseEvent event) {
-        if (play) {
-            btnPlay.setImage(new Image(ICON_PAUSE));
-            play = false;
-        } else {
-            btnPlay.setImage(new Image(ICON_PLAY));
-            play = true;
-        }
-    }
-
-    @FXML
-    void onActionRadio(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onActionRandom(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onActionRepeat(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onActionTimeline(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onActionVolume(MouseEvent event) {
-
-    }
-
-    private void agregarOpcionesBiblioteca() {
-        Label lbBiblioteca = new Label("Biblioteca");
-        Label lbCanciones = new Label("Canciones");
-        Label lbArtistas = new Label("Artistas");
-        Label lbAlbumes = new Label("Álbumes");
-        try {
-            lbCanciones.setGraphic(new ImageView(new Image(new FileInputStream(ICON_CANCIONES))));
-            lbArtistas.setGraphic(new ImageView(new Image(new FileInputStream(ICON_ARTISTAS))));
-            lbAlbumes.setGraphic(new ImageView(new Image(new FileInputStream(ICON_ALBUMES))));
-
-            lbBiblioteca.setFont(new Font("Avenir Book", 16));
-            lbCanciones.setFont(new Font("Avenir Book", 14));
-            lbArtistas.setFont(new Font("Avenir Book", 14));
-            lbAlbumes.setFont(new Font("Avenir Book", 14));
-
-            listOpciones.getItems().add(lbBiblioteca);
-            listOpciones.getItems().add(lbCanciones);
-            listOpciones.getItems().add(lbArtistas);
-            listOpciones.getItems().add(lbAlbumes);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Nota: En el futuro debe recibir una lista cargada con las playlist
-     */
-    private void agregarPlaylists() {
-        Label lbPlaylist = new Label("Playlist");
-        lbPlaylist.setFont(new Font("Avenir Book", 16));
-        listOpciones.getItems().add(lbPlaylist);
-
-        //AQUI SE DEBERÍAN AGREGAR LAS PLAYLIST DEL USUARIO
-        Label lbCanciones = new Label("Carne asada");
-        Label lbArtistas = new Label("Estudiar");
-        Label lbAlbumes = new Label("Baño de burbujas");
-        try {
-            lbCanciones.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
-            lbArtistas.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
-            lbAlbumes.setGraphic(new ImageView(new Image(new FileInputStream(ICON_PLAYLIST))));
-
-            lbCanciones.setFont(new Font("Avenir Book", 14));
-            lbArtistas.setFont(new Font("Avenir Book", 14));
-            lbAlbumes.setFont(new Font("Avenir Book", 14));
-
-            listOpciones.getItems().add(lbCanciones);
-            listOpciones.getItems().add(lbArtistas);
-            listOpciones.getItems().add(lbAlbumes);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Muestra la ventana.
-     *
-     * @param loader el loader con la ruta de la ventana que se quiere cargar.
-     */
-    public static void mostrarVentana(FXMLLoader loader) {
-        try {
-            Stage stagePrincipal = new Stage();
-            Parent root = (Parent) loader.load();
-            Scene scene = new Scene(root);
-            stagePrincipal.setScene(scene);
-            stagePrincipal.setMaximized(true);
-            stagePrincipal.show();
-        } catch (IOException ex) {
-            Logger.getLogger(IUReproductorController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+  }
 }
